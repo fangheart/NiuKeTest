f2d4a70255d877f3ec15c94e65560957a60c8834
fangheart <397403564@qq.com>
2017-03-21 20:44:28 +0800

    15 subject done

--- src/SixFive/Model/Solution.java
+++ src/SixFive/Model/Solution.java
@@ -3,8 +3,22 @@ package SixFive.Model;
 /**
  * Created by FangHeart on 2017/3/19.
  */
+/*
+
+ */
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
 public class Solution {
     public static void main(String[] args){
 
     }
+    /*
+    
+     */
 }
