6fe27121675ff01d57a1f8f4bbf01ad72a9b3341
fjx988 <fangheart@fangheart.win>
2017-03-22 10:40:21 +0800

    16 subject test

--- src/SixFive/NumOfOne11/Solution.java
+++ src/SixFive/NumOfOne11/Solution.java
@@ -1,10 +1,37 @@
 package SixFive.NumOfOne11;
 
+import java.util.Vector;
+
 /**
  * Created by FangHeart on 2017/3/19.
+ */
+/*
+输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
  */
 public class Solution {
     public static void main(String[] args){
 
+    }
+    /*
+    正常解法
+     */
+    public int NumberOf1(int n) {
+        int count = 0;
+        if (n==0)
+            return 0;
+        else {
+            while (n != 0) {
+                n = n - 1;
+                n = n & (n-1);
+                count++;
+            }
+        }
+        return count;
+    }
+    /*
+    用java工具类
+     */
+    public int NumberOf12(int n) {
+        return Integer.toBinaryString(n).replace("0","").length();
     }
 }
